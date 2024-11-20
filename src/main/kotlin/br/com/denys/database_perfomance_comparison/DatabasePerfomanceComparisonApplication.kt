00774c88be77c92f7e8a78de0c242a52abce7e80
Denys Maiolli <denysmaiolli@hotmail.com>
2024-11-19 21:29:25 -0300

    base setup

---
+++ src/main/kotlin/br/com/denys/database_perfomance_comparison/DatabasePerfomanceComparisonApplication.kt
@@ -0,0 +1,11 @@
+package br.com.denys.database_perfomance_comparison
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatabasePerfomanceComparisonApplication
+
+fun main(args: Array<String>) {
+	runApplication<DatabasePerfomanceComparisonApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/denys/database_perfomance_comparison/DatabasePerfomanceComparisonApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.denys.database_perfomance_comparison
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatabasePerfomanceComparisonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
